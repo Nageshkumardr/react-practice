ef330f51a5fa8a8d6613a8ab98bbe1607e0c9a9c
Nageshkumardr <63265281+Nageshkumardr@users.noreply.github.com>
2023-03-07 22:05:32 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+
 import Nav from "./Nav"
 
-const App = () => {
-  return (
-    <div>
-      <Nav data="nagesh" password="123"/>
-    </div>
-  )
-}
+// const App = () => {
+//   return (
+//     <div>
+//       <Nav data="nagesh" password="123"/>
+//     </div>
+//   )
+// }
 
+// export default App
+
+import React, { Component } from 'react'
+
+ class App extends Component {
+  render() {
+    return (
+      <div>
+        <Nav data="Nagesh" password="456"/>
+      </div>
+    )
+  }
+}
 export default App
--- Nav.jsx
+++ Nav.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
 
-const Nav = (props) => {
-  return (
-    <div>
-      {props.data}   {props.password}
-    </div>
-  )
+
+// const Nav = (props) => {
+//   return (
+//     <div>
+//       {props.data}   {props.password}
+//     </div>
+//   )
+// }
+
+// export default Nav
+import React, { Component } from 'react'
+
+ class Nav extends Component {
+  render() {
+    return (
+      <div>
+        {this.props.data}     {this.props.password}
+      </div>
+    )
+  }
 }
-
 export default Nav
