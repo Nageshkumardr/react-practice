df5c1efc36d6469281f4128f72a243d962b3f19c
Nageshkumardr <63265281+Nageshkumardr@users.noreply.github.com>
2023-03-13 18:07:02 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,14 +1,16 @@
 
 import React from "react";
-import JSON from "./actorsDetails.json";
-import Actors from "./Actors";
+// import JSON from "./actorsDetails.json";
+// import Actors from "./Actors";
 import "./global.css"
+import Details from "./Details";
 
 
 const App = ()=>{
     return(
         <div>
-            <Actors data={JSON}/>
+            {/* <Actors data={JSON}/> */}
+            <Details/>
         </div>
     )
 }
